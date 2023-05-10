d2034186c6e592989f6bfbcfdbf9f5fd81463c0b
warlock7867 <chandur.nischal@gmail.com>
2023-05-10 16:40:05 +0530

    DP problems

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	dp "dsa/DP"
+	"fmt"
+	"log"
+	"time"
+)
 
+func main() {
+	start := time.Now()
+	fmt.Println(dp.BestSumRec(9, []int{3, 2, 4}))
+
+	log.Println("Execution Time: ", time.Since(start))
 }
